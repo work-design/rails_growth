8ecc543690a8e6553f36f8cfee58fa2a49050193
qinmingyuan <mingyuan0715@foxmail.com>
2018-12-19 15:27:40 +0800

    add migration

---
+++ db/migrate/20181219054841_rails_growth_init.rb
@@ -0,0 +1,29 @@
+class RailsGrowthInit < ActiveRecord::Migration[5.2]
+  def change
+
+    create_table :rewards do |t|
+      t.references :entity, polymorphic: true
+      t.decimal :amount, precision: 10, scale: 2
+      t.integer :lock_version
+      t.integer :incomes_count
+      t.integer :expenses_count
+      t.timestamps
+    end
+
+    create_table :reward_incomes do |t|
+      t.references :reward
+      t.string :type
+      t.decimal :amount, precision: 10, scale: 2
+      t.timestamps
+    end
+
+    create_table :reward_expenses do |t|
+      t.references :reward
+      t.references :aim_user
+      t.references :aim
+      t.decimal :amount, precision: 10, scale: 2
+      t.timestamps
+    end
+
+  end
+end
