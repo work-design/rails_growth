3a61a65af57982b0e0cd7c34a3e2489a2b29dbee
qinmingyuan <mingyuan0715@foxmail.com>
2019-01-24 11:50:58 +0800

    reward return able codes

--- app/controllers/growth/api/aim_logs_controller.rb
+++ app/controllers/growth/api/aim_logs_controller.rb
@@ -7,6 +7,10 @@ class Growth::Api::AimLogsController < Growth::Api::BaseController
 
   def create
     r = growth_log(params[:code])
+    rewardable_codes = []
+    unless r.blank?
+      rewardable_codes = r[0].entity.rewardable_codes(aim_user.id)
+    end
     if r.present?
       @reward_amount = r.select(&:rewarded).sum { |i| i.aim_entity.reward_amount }
     end
@@ -14,7 +18,8 @@ class Growth::Api::AimLogsController < Growth::Api::BaseController
     if @reward_amount.to_d > 0
       reward = {
         amount: @reward_amount,
-        code: 'success'
+        code: 'success',
+        rewardable_codes: rewardable_codes
       }
       render json: { aim_logs: r, reward: reward }, status: :created
     elsif r.present?
