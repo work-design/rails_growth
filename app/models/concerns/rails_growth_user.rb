38b54ef63dfb4ea0c1fdcd13f81d6e0d7555fae3
qinmingyuan <mingyuan0715@foxmail.com>
2019-02-13 14:45:14 +0800

    move coin resource to wallet

--- app/models/concerns/rails_growth_user.rb
+++ app/models/concerns/rails_growth_user.rb
@@ -1,24 +1,16 @@
 module RailsGrowthUser
   extend ActiveSupport::Concern
-  included do
-    has_one :coin
 
+  included do
     has_many :aim_users, dependent: :destroy
     has_many :aim_entities, dependent: :nullify
     has_many :aims, through: :aim_users
     has_many :aim_logs
     has_many :reward_expenses
-    has_many :coin_logs
-    has_many :coin_cashes
-    has_many :coin_wallets
   end
 
   def reward_amount
     self.reward_expenses.sum(:amount)
-  end
-
-  def coin
-    super ? super : create_coin
   end
 
 end
