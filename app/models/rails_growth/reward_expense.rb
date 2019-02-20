703bf0eaab4428ae25fc49fbebad89291e2e4b51
qinmingyuan <mingyuan0715@foxmail.com>
2019-02-20 17:53:45 +0800

    error logs

--- app/models/rails_growth/reward_expense.rb
+++ app/models/rails_growth/reward_expense.rb
@@ -17,6 +17,7 @@ class RewardExpense < ApplicationRecord
       reward.save!
     else
       reward.errors.add :expense_amount, 'not equal'
+      logger.debug "Reward: #{reward.errors.full_messages.join(', ')}"
       raise ActiveRecord::RecordInvalid.new(reward)
     end
   end
@@ -32,6 +33,7 @@ class RewardExpense < ApplicationRecord
       coin.save!
     else
       coin.errors.add :income_amount, 'not equal'
+      logger.debug "Coin: #{coin.errors.full_messages.join(', ')}"
       raise ActiveRecord::RecordInvalid.new(coin)
     end
   end
