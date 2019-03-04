4ccea9630c19a3a430f98f82f4f2804cce2a4711
qinmingyuan <mingyuan0715@foxmail.com>
2019-03-04 14:02:29 +0800

    reset amount

--- app/models/rails_growth/reward_income.rb
+++ app/models/rails_growth/reward_income.rb
@@ -5,6 +5,7 @@ class RewardIncome < ApplicationRecord
   attribute :reward_amount, :decimal, default: 0
   validates :reward_amount, numericality: { greater_than_or_equal_to: 0 }
   after_create :sync_to_reward, if: -> { saved_change_to_reward_amount? }
+  after_update :reset_to_reward, if: -> { saved_change_to_reward_amount? }
 
   def sync_to_reward
     reward.reload
@@ -17,6 +18,11 @@ class RewardIncome < ApplicationRecord
       logger.error "#{self.class.name}/Reward: #{reward.errors.full_messages.join(', ')}"
       raise ActiveRecord::RecordInvalid.new(reward)
     end
+  end
+
+  def reset_to_reward
+    reward.income_amount = reward.compute_income_amount
+    reward.save!
   end
 
 end
