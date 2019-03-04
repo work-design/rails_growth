a4a89078e4135a7797225aad727a33c5d90802e4
qinmingyuan <mingyuan0715@foxmail.com>
2019-03-04 13:58:21 +0800

    use after create callback

--- app/models/rails_growth/reward_income.rb
+++ app/models/rails_growth/reward_income.rb
@@ -4,15 +4,16 @@ class RewardIncome < ApplicationRecord
 
   attribute :reward_amount, :decimal, default: 0
   validates :reward_amount, numericality: { greater_than_or_equal_to: 0 }
-  after_save :sync_to_reward, if: -> { saved_change_to_reward_amount? }
+  after_create :sync_to_reward, if: -> { saved_change_to_reward_amount? }
 
   def sync_to_reward
     reward.reload
     reward.income_amount += self.reward_amount
-    if reward.income_amount == reward.compute_income_amount
+    compute_amount = reward.compute_income_amount
+    if reward.income_amount == compute_amount
       reward.save!
     else
-      reward.errors.add :reward_amount, 'not equal'
+      reward.errors.add :reward_amount, "#{reward.income_amount} not equal #{compute_amount}"
       logger.error "#{self.class.name}/Reward: #{reward.errors.full_messages.join(', ')}"
       raise ActiveRecord::RecordInvalid.new(reward)
     end
