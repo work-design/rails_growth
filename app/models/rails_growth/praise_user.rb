c5bd7e478b445f2c9c5913c3076b2ea2b2a3e9ad
qinmingyuan <mingyuan0715@foxmail.com>
2019-03-06 10:24:10 +0800

    reset position

--- app/models/rails_growth/praise_user.rb
+++ app/models/rails_growth/praise_user.rb
@@ -5,14 +5,21 @@ class PraiseUser < ApplicationRecord
   belongs_to :user
   belongs_to :reward
   has_many :praise_incomes, ->(o){ where(reward_id: o.reward_id) }, foreign_key: :user_id, primary_key: :user_id
+  has_many :same_praise_users, ->(o){ where(reward_id: o.reward_id, entity_type: o.entity_type, entity_id: o.entity_id) }, class_name: 'PraiseUser', foreign_key: :user_id, primary_key: :user_id
 
   after_initialize if: :new_record? do
     self.entity_type = reward.entity_type
     self.entity_id = reward.entity_id
   end
+  after_commit :reset_position
 
   def compute_amount
     self.praise_incomes.sum(:amount)
+  end
+
+  def reset_position
+    lower_count = same_praise_users.default_where('amount-lt': self.amount).count
+    self.insert_at(lower_count)
   end
 
   def self.reset_position
