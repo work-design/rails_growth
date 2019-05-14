feca3c8ed6abe9a114521ef12239af46456998ea
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-14 15:14:22 +0800

    fix load err

--- lib/rails_growth.rb
+++ lib/rails_growth.rb
@@ -1,6 +1,2 @@
 require_relative 'rails_growth/engine'
 require_relative 'rails_growth/config'
-
-module RailsGrowth
-  # Your code goes here...
-end
--- lib/rails_growth/engine.rb
+++ lib/rails_growth/engine.rb
@@ -2,11 +2,6 @@ require 'rails_com'
 module RailsGrowth
   class Engine < ::Rails::Engine
 
-    config.eager_load_paths += Dir[
-      "#{config.root}/app/models/rails_growth",
-      "#{config.root}/app/models/rails_growth/concerns"
-    ]
-
     config.generators do |g|
       g.rails = {
         assets: false,
