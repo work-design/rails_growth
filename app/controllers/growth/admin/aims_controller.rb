36558bb655e4382737927f185379d744b1957538
qinmingyuan <mingyuan0715@foxmail.com>
2024-03-06 22:26:21 +0800

    admin in aim pange

--- app/controllers/growth/admin/aims_controller.rb
+++ app/controllers/growth/admin/aims_controller.rb
@@ -1,24 +1,14 @@
 module Growth
   class Admin::AimsController < Admin::BaseController
-    before_action :set_aim, only: [:show, :edit, :update, :destroy]
+    before_action :set_aim, only: [:show, :edit, :update, :destroy, :actions]
+    before_action :set_new_aim, only: [:new, :create]
 
     def index
       @aims = Aim.includes(:aim_codes).order(id: :desc).page(params[:page])
     end
 
     def new
-      @aim = Aim.new
       @aim.aim_codes.build
-    end
-
-    def create
-      @aim = Aim.new(aim_params)
-
-      if @aim.save
-        redirect_to admin_aims_url, notice: 'Aim was successfully created.'
-      else
-        render :new
-      end
     end
 
     def edit
@@ -27,17 +17,13 @@ module Growth
       end
     end
 
-    def add_item
-      @aim = Aim.new
-      @aim.aim_codes.build
-    end
-
-    def remove_item
-    end
-
     private
     def set_aim
       @aim = Aim.find(params[:id])
+    end
+
+    def set_new_aim
+      @aim = Aim.new(aim_params)
     end
 
     def aim_params
