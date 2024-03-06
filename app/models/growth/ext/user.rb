db354b87b5369c5b648def3eaf6ac1b6b8a69a11
qinmingyuan <mingyuan0715@foxmail.com>
2024-03-06 21:37:41 +0800

    update aims

--- app/models/growth/ext/user.rb
+++ app/models/growth/ext/user.rb
@@ -3,12 +3,12 @@ module Growth
     extend ActiveSupport::Concern
 
     included do
-      has_many :aim_users, dependent: :destroy
-      has_many :aim_entities, dependent: :nullify
-      has_many :aim_logs
-      has_many :reward_expenses
+      has_many :aim_users, class_name: 'Growth::AimUser', dependent: :destroy
+      has_many :aim_entities, class_name: 'Growth::AimEntity', dependent: :nullify
+      has_many :aim_logs, class_name: 'Growth::AimLog'
+      has_many :reward_expenses, class_name: 'Growth::RewardExpense'
 
-      has_many :aims, through: :aim_users
+      has_many :aims, class_name: 'Growth::Aim', through: :aim_users
     end
 
     def reward_amount
